96a37aac4ef304bf48438140864dcf84686ecd02
dani-lira-dev <73200886+dani-lira-dev@users.noreply.github.com>
2023-06-13 00:25:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CadastroSimples
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-xjww5z)
---
+++ script.js
@@ -0,0 +1,51 @@
+class Validator {
+
+  constructor() {
+    this.validations = [
+      'data-min-length',
+      'data-max-length',
+      'data-only-letters',
+      'data-email-validate',
+      'data-required',
+      'data-equal',
+      'data-password-validate',
+    ]
+  }
+
+  // inicia a validação de todos os campos
+  validate(form) {
+
+    // limpa todas as validações antigas
+    let currentValidations = document.querySelectorAll('form .error-validation');
+
+    if(currentValidations.length) {
+      this.cleanValidations(currentValidations);
+    }
+
+    // pegar todos inputs
+    let inputs = form.getElementsByTagName('input');
+    // transformar HTMLCollection em arr
+    let inputsArray = [...inputs];
+
+    // loop nos inputs e validação mediante aos atributos encontrados
+    inputsArray.forEach(function(input, obj) {
+
+      // fazer validação de acordo com o atributo do input
+      for(let i = 0; this.validations.length > i; i++) {
+        if(input.getAttribute(this.validations[i]) != null) {
+
+          // limpa string para saber o método
+          let method = this.validations[i].replace("data-", "").replace("-", "");
+
+          // valor do input
+          let value = input.getAttribute(this.validations[i])
+
+          // invoca o método
+          this[method](input,value);
+
+        }
+      }
+
+    }, this);
+
+  }
